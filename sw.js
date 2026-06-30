1d8a6a3ad5e07afa4ca1bc719734269ba94ea946
Guahh <t66305218@gmail.com>
2026-06-30 11:12:18 +1000

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Basic Service Worker to enable PWA installation
-self.addEventListener('fetch', (event) => {
-    // Standard fetch event listener
-    event.respondWith(
-        fetch(event.request).catch(() => {
-            return new Response('Network error occurred');
-        })
-    );
-});
